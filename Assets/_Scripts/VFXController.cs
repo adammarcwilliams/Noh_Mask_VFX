78af054e620a6be8033e9f0e1d6b0d166636a2b6
Adam Marc Williams <marc@rhythm.digital>
2019-05-04 12:38:47 +0100

    add VFXController and sets kitsune_disperse float in VFXGraph by script

---
+++ Assets/_Scripts/VFXController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.VFX;
+
+public class VFXController : MonoBehaviour
+{
+    [Range(0, 100)]
+    [SerializeField]
+    private float kitsuneDisperse = 0;
+
+    private VisualEffect visualEffect;
+
+
+    void Start()
+    {
+        visualEffect = GetComponent<VisualEffect>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        visualEffect.SetFloat("kitsune_disperse", kitsuneDisperse);
+    }
+}
