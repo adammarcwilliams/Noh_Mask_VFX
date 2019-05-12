b802e70818a809d6b41ee0be0fe0d64d159e630c
Adam Marc Williams <marc@rhythm.digital>
2019-05-12 16:59:58 +0100

    iterated on graph with new disperse effect to fix bug with update forces conflicting

--- Assets/_Scripts/MasksController.cs
+++ Assets/_Scripts/MasksController.cs
@@ -55,17 +55,17 @@ public class MasksController : MonoBehaviour
 
     private IEnumerator TransitionNextMask()
     {
-        // disperse current mask offscreen left
+        // disperse current mask using turbulence
         string currentMask = masks[maskIndex].MaskName;
-        visualEffect.SetFloat("disperse", -10f);
-        yield return new WaitForSeconds(1f);
-        // reinit visual effect
-        visualEffect.Reinit();
+        visualEffect.SetFloat("intensity", 50f);
+        visualEffect.SendEvent($"OnStop{currentMask}");
+        yield return new WaitForSeconds(1.5f);
+
         // update currentmask to next mask
         maskIndex = (maskIndex + 1) % masks.Length;
         string nextMask = masks[maskIndex].MaskName;
-        // make sure disperse is 0 on next mask
-        visualEffect.SetFloat("disperse", 0f);
+        // reset insensity to default
+        visualEffect.SetFloat("intensity", 0.2f);
         // play next mask
         visualEffect.SendEvent($"OnPlay{nextMask}");
         yield return null;
@@ -73,12 +73,12 @@ public class MasksController : MonoBehaviour
 
     private IEnumerator TransitionPreviousMask()
     {
-        // disperse current mask offscreen right
+        // disperse current mask using turbulence
         string currentMask = masks[maskIndex].MaskName;
-        visualEffect.SetFloat("disperse", 10f);
-        yield return new WaitForSeconds(1f);
-        // reinit visual effect
-        visualEffect.Reinit();
+        visualEffect.SetFloat("intensity", 50f);
+        visualEffect.SendEvent($"OnStop{currentMask}");
+        yield return new WaitForSeconds(1.5f);
+
         // update currentmask to previous mask
         if (maskIndex == 0)
         {
@@ -90,8 +90,8 @@ public class MasksController : MonoBehaviour
         }
 
         string previousMask = masks[maskIndex].MaskName;
-        // make sure disperse is 0 on next mask
-        visualEffect.SetFloat("disperse", 0f);
+        // reset insensity to default
+        visualEffect.SetFloat("disperse", 0.2f);
         // play previous mask
         visualEffect.SendEvent($"OnPlay{previousMask}");
         yield return null;
